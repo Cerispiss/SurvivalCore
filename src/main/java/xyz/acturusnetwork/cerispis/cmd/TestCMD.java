4b6ece3e82340fb04186e5237a7660306bb92f05
ZexiscMC <100186596+ZexiscMC@users.noreply.github.com>
2023-06-02 20:23:40 +0700

    h

---
+++ src/main/java/xyz/acturusnetwork/cerispis/cmd/TestCMD.java
@@ -0,0 +1,16 @@
+package xyz.acturusnetwork.cerispis.cmd;
+
+import org.bukkit.command.CommandSender;
+import xyz.acturusnetwork.cerispis.api.ACommand;
+
+public class TestCMD extends ACommand {
+
+    public TestCMD() {
+        super("test");
+    }
+
+    @Override
+    public void run(CommandSender sender, String label, String[] args) {
+        sender.sendMessage("test");
+    }
+}
